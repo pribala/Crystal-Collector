887e60b3a59845e703f40a6f2ba56098d9f60a7d
Priya Balakrishnan <andu_pri@yahoo.com>
2017-08-18 23:15:32 -0400

    fix: Add sound effect for win and loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,53 +7,71 @@ var crystals = [{name: "Crystal-1", src: "assets/images/crystal1.jpg", power: 0}
 function generateNumber(min, max) {
 	return Math.floor(Math.random() * max) + min;
 }
+
+var audioElement1 = new Audio("assets/audio/person_cheering.wav");
+var audioElement2 = new Audio("assets/audio/End_Fx.wav");
 var target = 0;
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
 
 $(document).ready(function() {
-	crystals.forEach(function(obj) {
-	 	obj.power = generateNumber(1, 12);
-	 	var newDiv = $("<div>");
-	 	newDiv.addClass("col-sm-3");
-	 	//var newImg = $("<img>");
-		// newImg.attr("src", obj.src);
-		// newDiv.append(newImg);
-		// newImg.addClass("img img-responsive");
-		// newImg.attr("data-attribute", obj.power);
-		var newBtn = $("<button>");
-		newBtn.css({"width": "250px", "height": "202px"});
-		newBtn.css({"background-image": "url("+obj.src+")"});
-		newBtn.attr("data-attribute", obj.power);
-		newDiv.append(newBtn);
-		$("#image-container").append(newDiv);
-	});
+	// crystals.forEach(function(obj) {
+	//  	obj.power = generateNumber(1, 12);
+	//  	var newDiv = $("<div>");
+	//  	newDiv.addClass("col-md-3 col-sm-6 col-xs-12");
+	//  	var newBtn = $("<button>");
+	// 	newBtn.css({"width": "250px", "height": "202px"});
+	// 	newBtn.css({"background-image": "url("+obj.src+")"});
+	// 	newBtn.attr("data-attribute", obj.power);
+	// 	newDiv.append(newBtn);
+	// 	$("#image-container").append(newDiv);
+		powerUp();
+	
 	target = generateNumber(19,120);
 	$("#targetNum").text(target);
 	
 	$("#wins").text(wins);
 	$("#losses").text(losses);
 	$("#totalScore").text(totalScore);
-
-	$("button").on("click", function(){
+	$("body").on("click", ".crystalBtn", function(){
 		totalScore += parseInt($(this).attr("data-attribute"));
 		$("#totalScore").text(totalScore);
 		if(totalScore === target) {
 			wins++;
 			$("#wins").text(wins);
+			audioElement1.play();
 			resetGame();
 		} else if(totalScore > target) {
 			losses++;
 			$("#losses").text(losses);
+			audioElement2.play();
 			resetGame();
 		}
 	});
 
 	function resetGame() {
+		powerUp();
 		target = generateNumber(19,120);
 		$("#targetNum").text(target);
 		totalScore = 0;
 		$("#totalScore").text(totalScore);
+	}
+
+	function powerUp() {
+		$("#image-container").empty();
+		crystals.forEach(function(obj) {
+	 	obj.power = generateNumber(1, 12);
+	 	var newDiv = $("<div>");
+	 	newDiv.addClass("col-md-3 col-sm-6 col-xs-12");
+	 	var newBtn = $("<button>");
+	 	newBtn.addClass("crystalBtn");
+		newBtn.css({"width": "250px", "height": "202px"});
+		newBtn.css({"background-image": "url("+obj.src+")"});
+		newBtn.attr("data-attribute", obj.power);
+		newDiv.append(newBtn);
+		$("#image-container").append(newDiv);	
+		});
+		
 	}
 });
